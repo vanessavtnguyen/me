ad48cef1456d7a4d9f7659e92102b8a8f96b880b
vanessa <vanessavtnguyen@gmail.com>
2020-06-10 13:20:04 +1000

    Lab Week 2 Exercise 2

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -15,7 +15,7 @@ def getLetter(index):
 
 def week2exersise2():
     indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
-    wordArray = map(getLetter, indices)
+    wordArray = list(map(getLetter, indices))
     wordArray[0] = wordArray[0].upper()
     wordArray[1] = wordArray[1].upper()
     wordArray[3] = wordArray[3].upper()
